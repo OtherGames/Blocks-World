b43782ed3a234a0ec2dfb11e696a088ec49bc1d4
OtherStickmanGames <other.stickman.games@gmail.com>
2024-05-15 15:35:45 +0300

    Rename

--- Builds/UI/BuildSaveModeHandler.cs
+++ Builds/UI/BuildSaveModeHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildSaveModeHanfler : MonoBehaviour
+public class BuildSaveModeHandler : MonoBehaviour
 {
     [SerializeField] GameObject indicator;
 
